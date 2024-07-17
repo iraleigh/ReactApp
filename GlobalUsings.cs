8530b82914832340e4d7971e1708279ef3541bd3
Iain Raleigh <iainraleigh@Iains-MBP.attlocal.net>
2024-07-16 21:54:34 -0700

    Initialize React App

---
+++ GlobalUsings.cs
@@ -0,0 +1,3 @@
+global using Ardalis.GuardClauses;
+global using ReactApp.Web.Infrastructure;
+global using MediatR;
---
+++ bin/Debug/net8.0/93a9a2501b3947698e8dc1b993b68a84.sh
@@ -0,0 +1,30 @@
+function list_child_processes () {
+    local ppid=$1;
+    local current_children=$(pgrep -P $ppid);
+    local local_child;
+    if [ $? -eq 0 ];
+    then
+        for current_child in $current_children
+        do
+          local_child=$current_child;
+          list_child_processes $local_child;
+          echo $local_child;
+        done;
+    else
+      return 0;
+    fi;
+}
+
+ps 11577;
+while [ $? -eq 0 ];
+do
+  sleep 1;
+  ps 11577 > /dev/null;
+done;
+
+for child in $(list_child_processes 12006);
+do
+  echo killing $child;
+  kill -s KILL $child;
+done;
+rm /Users/iainraleigh/Code/ReactApp/src/Web/bin/Debug/net8.0/93a9a2501b3947698e8dc1b993b68a84.sh;
